3c5c027897620d6c73115e6afbaf784f420cc1ca
samarthisworking <samarthisworking@gmail.com>
2026-03-22 14:56:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,29 +9,43 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// DB connect
 mongoose.connect(process.env.MONGO_URI)
-.then(()=>console.log("DB Connected"));
+.then(()=>console.log("DB Connected"))
+.catch(err=>console.log(err));
 
+// Model
 const Employee = mongoose.model("Employee", {
   name:String
 });
 
+// Test route
 app.get("/", (req,res)=>{
   res.send("API Running");
 });
 
+// Add employee
 app.post("/add", async(req,res)=>{
-  await Employee.create({name:req.body.name});
-  res.send("Added");
+  try{
+    await Employee.create({name:req.body.name});
+    res.send("Added");
+  }catch(e){
+    res.status(500).send(e.message);
+  }
 });
 
+// Get all employees
 app.get("/all", async(req,res)=>{
-  const data = await Employee.find();
-  res.json(data);
+  try{
+    const data = await Employee.find();
+    res.json(data);
+  }catch(e){
+    res.status(500).send(e.message);
+  }
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, ()=>{
-  console.log("Server running");
+  console.log("Server running on " + PORT);
 });
